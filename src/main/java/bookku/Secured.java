92063c0f8981eb19ea3d0201ba5dce6f62693a2c
Paweł Cegła <3962186+pawelcegla@users.noreply.github.com>
2024-12-19 13:25:53 +0100

    [WiP] Small changes

--- src/main/java/bookku/Secured.java
+++ src/main/java/bookku/Secured.java
@@ -1,10 +1,11 @@
 package bookku;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
 import static org.springframework.http.MediaType.TEXT_HTML_VALUE;
@@ -13,14 +14,16 @@ import static org.springframework.http.MediaType.TEXT_HTML_VALUE;
 @RequestMapping("/__/b")
 public class Secured {
 
+    private static final Logger log = LoggerFactory.getLogger(Secured.class);
+
     @GetMapping(produces = TEXT_HTML_VALUE)
     public String secret() {
-        return "secured";
+        return "secured_form";
     }
 
     @PostMapping(consumes = APPLICATION_FORM_URLENCODED_VALUE)
-    @ResponseBody
     public String create(Slug slug, Target target) {
-        return String.format("%s -> %s", slug, target);
+        log.info("Form submitted: '{}' -> '{}'", slug.value(), target.value());
+        return String.format("redirect:%s", target.value());
     }
 }
