76c46d4cb3110aaf1a788fac0141ec8e0aa47dcb
Paweł Cegła <3962186+pawelcegla@users.noreply.github.com>
2025-02-15 19:26:59 +0100

    Generate a placeholder link

--- src/main/java/bookku/Crud.java
+++ src/main/java/bookku/Crud.java
@@ -3,9 +3,12 @@ package bookku;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
 
@@ -22,7 +25,8 @@ public class Crud {
     }
 
     @GetMapping
-    public String form() {
+    public String form(WebRequest req, UriComponentsBuilder builder, Model model) {
+        model.addAttribute("asdf", builder.path("asdf").build());
         return "form";
     }
 
